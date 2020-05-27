6e74bb988ef0e016a57a04a5cc9430bfd50755c7
Sol Jennings <sol.jennings@myob.com>
2020-05-27 12:31:08 +1000

    hex

--- Hive.FrontEnd/src/js/__tests__/Hextille.test.tsx
+++ Hive.FrontEnd/src/js/__tests__/Hextille.test.tsx
@@ -1,12 +1,36 @@
 import {render} from '@testing-library/preact';
 import React from 'preact/compat';
+import Hextille from '../components/Hextille';
+import {useHiveContext} from '../game-context';
+import Mock = jest.Mock;
 
-describe('', () => {
-    beforeEach(() => {
-        render(<div/>);
+jest.mock('../game-context');
+
+const createCell = (q: number, r: number) => ({coordinates: {q, r}, tiles: []});
+const create = (...coordinates: [number, number][]) => {
+    (useHiveContext as Mock).mockReturnValue(({
+        hexagons: coordinates.map(([q, r]) => createCell(q, r)),
+        players: jest.fn(),
+        moveTile: jest.fn()
+    }));
+    render(<Hextille/>).container.firstElementChild;
+    const rows = document.body.getElementsByClassName('hex-row');
+    const cells = document.body.getElementsByClassName('cell');
+    const hidden = document.body.getElementsByClassName('hidden');
+ 
+    return [rows, cells, hidden] as const;
+};
+
+describe('Hextille', () => {
+    test('can be created with 1 cell', () => {
+        const [rows, cells] = create([0,0]);
+        
+        expect(cells).toHaveLength(1);
+        expect(rows).toHaveLength(1);
     });
-
-    test('', () => {
-        expect(true).toBeTruthy();
+    
+    test('r increases rows', () => {
+        const [rows] = create([0,0],[0,1],[0,2]);
+        expect(rows).toHaveLength(3);
     });
 });
