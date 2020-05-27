73e1fa45c5161fb5c5cf2cf889337edde3baed83
Sol Jennings <syntheticexistence@gmail.com>
2020-05-27 18:36:04 +1000

    sort cells

--- Hive.FrontEnd/src/js/__tests__/Hextille.test.tsx
+++ Hive.FrontEnd/src/js/__tests__/Hextille.test.tsx
@@ -1,12 +1,12 @@
 import {render} from '@testing-library/preact';
-import React from 'preact/compat';
+import { h } from 'preact';
 import Hextille from '../components/Hextille';
 import {useHiveContext} from '../game-context';
 import Mock = jest.Mock;
 
 jest.mock('../game-context');
 
-const createCell = (q: number, r: number) => ({coordinates: {q, r}, tiles: []});
+const createCell = (q: number, r: number) => ({coordinates: {q, r}, tiles: [{content:q+'-'+r, tileId:0,playerId:0,availableMoves:[]}]});
 const create = (...coordinates: [number, number][]) => {
     (useHiveContext as Mock).mockReturnValue(({
         hexagons: coordinates.map(([q, r]) => createCell(q, r)),
@@ -17,20 +17,34 @@ const create = (...coordinates: [number, number][]) => {
     const rows = document.body.getElementsByClassName('hex-row');
     const cells = document.body.getElementsByClassName('cell');
     const hidden = document.body.getElementsByClassName('hidden');
- 
+
     return [rows, cells, hidden] as const;
 };
 
 describe('Hextille', () => {
     test('can be created with 1 cell', () => {
         const [rows, cells] = create([0,0]);
-        
+
         expect(cells).toHaveLength(1);
         expect(rows).toHaveLength(1);
     });
-    
+
     test('r increases rows', () => {
         const [rows] = create([0,0],[0,1],[0,2]);
         expect(rows).toHaveLength(3);
+    });
+
+    test('cells are sorted', () => {
+        const [,cells] = create([2,0],[1,0],[3,0]);
+        expect(cells[0].textContent).toBe('1-0');
+        expect(cells[1].textContent).toBe('2-0');
+        expect(cells[2].textContent).toBe('3-0');
+    });
+
+    test('rows are sorted', () => {
+        const [rows] = create([0,3],[0,1],[0,2]);
+        expect(rows[0].textContent).toBe('0-1');
+        expect(rows[1].textContent).toBe('0-2');
+        expect(rows[2].textContent).toBe('0-3');
     });
 });
