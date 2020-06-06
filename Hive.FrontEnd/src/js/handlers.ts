5a16bee53c807c5071d9b16c7e85301aa85142e1
Sol Jennings <sol.jennings@myob.com>
2020-06-06 23:11:27 +1000

    move test folder

--- Hive.FrontEnd/src/js/handlers.ts
+++ Hive.FrontEnd/src/js/handlers.ts
@@ -3,7 +3,7 @@ export function handleDragOver(ev: { preventDefault: () => void }): boolean {
     return false;
 }
 
-export function handleDrop(ev: { preventDefault: () => void }) {
+export function handleDrop(ev: { preventDefault: () => void }): boolean {
     ev.preventDefault();
     return false;
 }
