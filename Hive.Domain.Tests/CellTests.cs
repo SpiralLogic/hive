4ac07feecb61e16d3dc6e6b8921e7aac81ffeea1
Sol Jennings <sol.jennings@myob.com>
2020-09-13 11:38:02 +1000

    add test project

---
+++ Hive.Domain.Tests/CellTests.cs
@@ -0,0 +1,26 @@
+﻿using Hive.Domain.Entities;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Hive.Domain.Tests
+{
+    public class CellTests
+    {
+        [Fact]
+        public void StartWithNoTiles()
+        {
+            var cell = new Cell( new Coords(1, 1));
+
+            Assert.Empty(cell.Tiles);
+        }
+        
+        [Fact]
+        public void CanStartWithTiles()
+        {
+            var cell = new Cell(new Coords(1, 1)) { Tiles = new HashSet<Tile> { new Tile(1, 2, "tile1"), new Tile(1, 2, "tile2") } };
+
+            Assert.Equal(2, cell.Tiles.Count);
+        }
+
+    }
+}
---
+++ Hive.Domain.Tests/CoordinatesTests.cs
@@ -0,0 +1,48 @@
+﻿using Hive.Domain.Entities;
+using Xunit;
+
+namespace Hive.Domain.Tests
+{
+    public class CoordinateTests
+    {
+
+        [Fact]
+        public void CoordsAreEqual()
+        {
+            var coord1 = new Coords(1, 1);
+            var coord2 = new Coords(1, 1);
+
+            Assert.Equal(coord1, coord2);
+        }
+
+
+        [Fact]
+        public void CoordsAreNotEqual()
+        {
+            var coord1 = new Coords(1, 1);
+            var coord2 = new Coords(1, 2);
+
+            Assert.NotEqual(coord1, coord2);
+        }
+
+        [Fact]
+        public void CoordsAretEqualWith()
+        {
+            var coord1 = new Coords(1, 1);
+            var coord2 = new Coords(1, 2);
+            var coord3 = coord2 with { R = 1 };
+
+
+            Assert.Equal(coord1, coord3);
+        }
+
+        [Fact]
+        public void CoordsAreNotEqualWith()
+        {
+            var coord1 = new Coords(1, 1);
+            var coord2 = coord1 with { R = 2 };
+
+            Assert.NotEqual(coord1, coord2);
+        }
+    }
+}
