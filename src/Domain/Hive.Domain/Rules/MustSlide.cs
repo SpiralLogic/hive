e345451cf4438f8560e898108c52d609da05f408
Sol Jennings <sol.jennings@myob.com>
2020-10-05 06:19:12 +1100

    Must slide rule

--- src/Domain/Hive.Domain/Rules/MustSlide.cs
+++ src/Domain/Hive.Domain/Rules/MustSlide.cs
@@ -1,14 +1,33 @@
-﻿using System;
+﻿using System.Linq;
 using System.Collections.Generic;
 using Hive.Domain.Entities;
+using Hive.Domain.Extensions;
 
 namespace Hive.Domain.Rules
 {
     public class MustSlide : IRule
     {
+        private readonly ISet<Cell> allEmptyCells = new HashSet<Cell>();
+        private readonly ISet<Cell> checkd = new HashSet<Cell>();
+
         public ISet<Coords> ApplyRule(Cell originCell, ISet<Cell> allCells)
         {
-            throw new NotImplementedException();
+            allEmptyCells.UnionWith(allCells.WhereEmpty());
+            return GetSlidableNeighbors(originCell).ToCoords();
+        }
+
+        private IEnumerable<Cell> GetSlidableNeighbors(Cell start)
+        {
+            checkd.Add(start);
+            var neighbors = allEmptyCells.SelectEmptyNeighbors(start);
+            var newSlideTo = neighbors.Where(n => CanSlideTo(n, neighbors));
+
+            return newSlideTo.Union(newSlideTo.Except(checkd).SelectMany(GetSlidableNeighbors));
+        }
+        
+        private static bool CanSlideTo(Cell end, IEnumerable<Cell> emptyNeighbors)
+        {
+            return emptyNeighbors.SelectEmptyNeighbors(end).Intersect(emptyNeighbors).Any();
         }
     }
 }
