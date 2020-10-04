c1f3b6e23979edb42d3c7223cd49aa89a13ea930
Sol Jennings <sol.jennings@myob.com>
2020-10-05 06:19:23 +1100

    next empty rule

---
+++ src/Domain/Hive.Domain.Tests/RuleTests/NextEmptyTests.cs
@@ -0,0 +1,73 @@
+﻿using Hive.Domain.Rules;
+using Hive.Domain.Tests.TestUtils;
+using Xunit;
+
+namespace Hive.Domain.Tests.RuleTests.Movements
+{
+    public class NextEmptyTests
+    {
+        [Fact]
+        public void CalculatesNextEmpty()
+        {
+            var initial = new InitialHiveBuilder();
+
+            initial += " ⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ";
+            initial += "⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ⬡";
+            initial += " ⬡ ⬡ ⬡ ⬡ ⬡ ⏣ ⬡ ⬡ ⬡ ";
+            initial += "⬡ ⬡ ⬡ ⬡ ⏣ ⏣ ⬡ ⬡ ⬡ ⬡";
+            initial += " ⬡ ⬡ ⏣ ⏣ ★ ⏣ ⏣ ⬡ ⬡ ";
+            initial += "⬡ ⬡ ⬡ ⬡ ⏣ ⏣ ⬡ ⬡ ⬡ ⬡";
+            initial += " ⬡ ⬡ ⬡ ⬡ ⬡ ⏣ ⬡ ⬡ ⬡ ";
+            initial += "⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ⬡";
+            initial += " ⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ";
+
+            var expected = new ExpectedHiveBuilder();
+
+            expected += " ⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ";
+            expected += "⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ✔ ⬡ ⬡ ⬡";
+            expected += " ⬡ ⬡ ⬡ ✔ ⬡ ⏣ ⬡ ⬡ ⬡ ";
+            expected += "⬡ ⬡ ⬡ ⬡ ⏣ ⏣ ⬡ ⬡ ⬡ ⬡";
+            expected += " ⬡ ✔ ⏣ ⏣ ★ ⏣ ⏣ ✔ ⬡ ";
+            expected += "⬡ ⬡ ⬡ ⬡ ⏣ ⏣ ⬡ ⬡ ⬡ ⬡";
+            expected += " ⬡ ⬡ ⬡ ✔ ⬡ ⏣ ⬡ ⬡ ⬡ ";
+            expected += "⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ✔ ⬡ ⬡ ⬡";
+            expected += " ⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ";
+
+            var rule = new NextEmpty();
+
+            rule.Should().HaveMoves(initial, expected);
+        }
+
+            [Fact]
+        public void DoesntIncludeAdjacent()
+        {
+            var initial = new InitialHiveBuilder();
+
+            initial += " ⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ";
+            initial += "⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ⬡";
+            initial += " ⬡ ⬡ ⬡ ⬡ ⬡ ⏣ ⬡ ⬡ ⬡ ";
+            initial += "⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ⬡";
+            initial += " ⬡ ⬡ ⏣ ⬡ ★ ⬡ ⏣ ⬡ ⬡ ";
+            initial += "⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ⬡";
+            initial += " ⬡ ⬡ ⬡ ⬡ ⬡ ⏣ ⬡ ⬡ ⬡ ";
+            initial += "⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ⬡";
+            initial += " ⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ";
+
+            var expected = new ExpectedHiveBuilder();
+
+            expected += " ⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ";
+            expected += "⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ⬡";
+            expected += " ⬡ ⬡ ⬡ ⬡ ⬡ ⏣ ⬡ ⬡ ⬡ ";
+            expected += "⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ⬡";
+            expected += " ⬡ ⬡ ⏣ ⬡ ★ ⬡ ⏣ ⬡ ⬡ ";
+            expected += "⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ⬡";
+            expected += " ⬡ ⬡ ⬡ ⬡ ⬡ ⏣ ⬡ ⬡ ⬡ ";
+            expected += "⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ⬡";
+            expected += " ⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ⬡ ";
+
+            var rule = new NextEmpty();
+
+            rule.Should().HaveMoves(initial, expected);
+        }
+    }
+}
---
+++ src/Domain/Hive.Domain/Rules/NextEmpty.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Hive.Domain.Entities;
+
+namespace Hive.Domain.Rules
+{
+    public class NextEmpty : IRule
+    {
+        public ISet<Coords> ApplyRule(Cell currentCell, ISet<Cell> allCells)
+        {
+            var coords = new HashSet<Coords>() {
+                GetFirstEmpty(allCells, (Coords c) => c.TopLeft(), currentCell.Coords) ,
+                GetFirstEmpty(allCells, (Coords c) => c.TopRight(), currentCell.Coords) ,
+                GetFirstEmpty(allCells, (Coords c) => c.Left(), currentCell.Coords) ,
+                GetFirstEmpty(allCells, (Coords c) => c.Right(), currentCell.Coords) ,
+                GetFirstEmpty(allCells, (Coords c) => c.BottomLeft(), currentCell.Coords) ,
+                GetFirstEmpty(allCells, (Coords c) => c.BottomRight(), currentCell.Coords) ,
+                };
+            coords.ExceptWith(currentCell.Coords.GetNeighbors());
+            return coords;
+        }
+
+        public Coords GetFirstEmpty(ISet<Cell> allCells, Func<Coords, Coords> next, Coords current)
+        {
+            var cell = allCells.First(c => c.Coords == current);
+            return cell.IsEmpty() ? current : GetFirstEmpty(allCells, next, next(current));
+        }
+    }
+}
