f752403c3b3db289f8fc85e4fc47187775b47540
Sol Jennings <sol.jennings@myob.com>
2020-10-05 06:19:36 +1100

    queens must be placed rule

---
+++ src/Domain/Hive.Domain.Tests/RuleTests/QueenMustBePlacedTests.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using Hive.Domain.Entities;
+using Hive.Domain.Rules;
+using Hive.Domain.Tests.TestUtils;
+using Xunit;
+
+namespace Hive.Domain.Tests.RuleTests
+{
+    public class QueenMustBePlacedTests
+    {
+        [Fact]
+        public void CantMoveWithNoQueen()
+        {
+            var cells = new HashSet<Cell> {
+                new Cell(new Coords(1, 1)).AddTile(new Tile(1, 1, Creatures.Beetle)),
+                 new Cell(new Coords(1, 2)).AddTile(new Tile(2, 1, Creatures.Beetle)),
+                new Cell(new Coords(1, 3)).AddTile(new Tile(3, 1, Creatures.Beetle)),
+            };
+
+            var rule = new QueenMustBePlaced();
+
+            rule.ApplyRule(cells.First(), cells).Should().BeEmpty();
+        }
+
+        [Fact]
+        public void CanMoveWhenQueenIsPlaced()
+        {
+            var cells = new HashSet<Cell> {
+                new Cell(new Coords(1, 1)).AddTile(new Tile(1, 1, Creatures.Beetle)),
+                 new Cell(new Coords(1, 2)).AddTile(new Tile(2, 1, Creatures.Beetle)),
+                new Cell(new Coords(1, 3)).AddTile(new Tile(3, 1, Creatures.Beetle)),
+                new Cell(new Coords(1, 4)).AddTile(new Tile(4, 1, Creatures.Queen)),
+            };
+
+            var rule = new QueenMustBePlaced();
+
+            rule.ApplyRule(cells.First(), cells).Should().NotBeEmpty();
+        }
+    }
+}
---
+++ src/Domain/Hive.Domain/Rules/QueenMustBePlaced.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Hive.Domain.Entities;
+using Hive.Domain.Extensions;
+
+namespace Hive.Domain.Rules
+{
+    public class QueenMustBePlaced : IRule
+    {
+        public ISet<Coords> ApplyRule(Cell currentCell, ISet<Cell> allCells)
+        {
+            return QueenHasBeenPlaced(currentCell, allCells) ? allCells.ToCoords() : new HashSet<Coords>();
+        }
+
+        private static bool QueenHasBeenPlaced(Cell currentCell, ISet<Cell> allCells)
+        {
+            return allCells
+                            .WhereOccupied()
+                            .SelectMany(c => c.Tiles)
+                            .Where(c => c.PlayerId == currentCell.TopTile().PlayerId)
+                            .Any(c => c.Creature == Creatures.Queen);
+        }
+    }
+}
