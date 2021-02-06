1634b35c689c80a175ca1c1604aaf34cfe065aad
Sol Jennings <sol.jennings@myob.com>
2021-02-07 01:52:50 +1100

    add share svg

--- src/FrontEnd/src/index.tsx
+++ src/FrontEnd/src/index.tsx
@@ -2,6 +2,7 @@ import 'preact/debug';
 import {h, render} from 'preact';
 import App from "./components/App";
 
-fetch('/svg/creatures.svg').then(r=>r.text()).then(svg => document.body.insertAdjacentHTML('beforeend', `<div class="creatures-svg">${svg}</div>`));
+fetch('/svg/creatures.svg').then(r=>r.text()).then(svg => document.body.insertAdjacentHTML('beforeend', `<div class="svg">${svg}</div>`));
+fetch('/svg/share.svg').then(r=>r.text()).then(svg => document.body.insertAdjacentHTML('beforeend', `<div class="svg">${svg}</div>`));
 render(h(App, {}), document.body);
 
