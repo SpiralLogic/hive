2606422183aa9653b359b0e59d39c2509d297986
Sol <sol.jennings@myob.com>
2021-02-07 04:25:26 +1100

    build

--- src/Api/Hive.Api/Startup.cs
+++ src/Api/Hive.Api/Startup.cs
@@ -23,7 +23,6 @@ namespace Hive
             _configuration = configuration;
         }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         [ExcludeFromCodeCoverage]
         public void ConfigureServices(IServiceCollection services)
