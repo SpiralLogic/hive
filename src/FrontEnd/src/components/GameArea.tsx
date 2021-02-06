7900848960eaf7cbec44fed252faef2df025af11
Sol <sol.jennings@myob.com>
2021-02-07 03:31:06 +1100

    build

--- src/FrontEnd/src/components/GameArea.tsx
+++ src/FrontEnd/src/components/GameArea.tsx
@@ -1,6 +1,5 @@
 import {FunctionComponent, h} from 'preact';
 import {GameState} from '../domain';
-import {JSXInternal} from "preact/src/jsx";
 import {handleDragOver} from '../handlers';
 import Hextille from './Hextille';
 import PlayerList from './PlayerList';
@@ -12,7 +11,6 @@ const GameArea: FunctionComponent<Props> = ({ gameState}) => {
     const attributes = {
         ondragover: handleDragOver,
         className: 'hive',
-        style: {'--hex-size': '4vh'} as JSXInternal.CSSProperties,
     };
     return (
         <div {...attributes}>
