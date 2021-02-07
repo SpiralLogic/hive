acd2591de7778b0426aa3f841eef2d2203307a76
Sol Jennings <sol.jennings@myob.com>
2021-02-07 23:06:04 +1100

    cache bust

--- src/FrontEnd/src/index.tsx
+++ src/FrontEnd/src/index.tsx
@@ -1,11 +1,28 @@
-if (process.env.NODE_ENV !== 'production') {// @ts-ignore
-    import ('preact/debug');
+if (process.env.NODE_ENV !== 'production') {
+  // @ts-ignore
+  import('preact/debug');
 }
 
 import { h, render } from 'preact';
 import App from './components/App';
+import './css/hive.css';
 
-fetch('/svg/creatures.svg').then(r => r.text()).then(svg => document.body.insertAdjacentHTML('beforeend', `<div class="svg">${svg}</div>`));
-fetch('/svg/share.svg').then(r => r.text()).then(svg => document.body.insertAdjacentHTML('beforeend', `<div class="svg">${svg}</div>`));
+fetch('/svg/creatures.svg')
+  .then((r) => r.text())
+  .then((svg) =>
+    document.body.insertAdjacentHTML(
+      'beforeend',
+      `<div class="svg">${svg}</div>`
+    )
+  );
+
+fetch('/svg/share.svg')
+  .then((r) => r.text())
+  .then((svg) =>
+    document.body.insertAdjacentHTML(
+      'beforeend',
+      `<div class="svg">${svg}</div>`
+    )
+  );
+
 render(h(App, {}), document.body);
-
