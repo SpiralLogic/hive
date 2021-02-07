0eef5b4b5a36bf5f03cd02de95beac3c8ade6996
Sol Jennings <sol.jennings@myob.com>
2021-02-07 23:23:35 +1100

    cache bust

--- src/FrontEnd/src/components/PlayerTiles.tsx
+++ src/FrontEnd/src/components/PlayerTiles.tsx
@@ -1,24 +1,32 @@
-import {FunctionComponent, h} from 'preact';
-import {Player} from '../domain';
-import {deepEqual} from 'fast-equals';
-import {memo} from 'preact/compat';
+import { FunctionComponent, h } from 'preact';
+import { Player } from '../domain';
+import { deepEqual } from 'fast-equals';
+import { memo } from 'preact/compat';
 import Tile from './Tile';
 
 type Props = Player;
 
 const PlayerTiles: FunctionComponent<Props> = (props: Props) => {
-    const {name, tiles, id} = props;
-    const changePlayerUrl = `${window.location.pathname.split('/').slice(0,3).join('/')}/${id}`
-    return tiles.length > 0 ? (
-        <div className="player" title={name}>
-            <a className="name" href={changePlayerUrl}>{name}</a>
-            <div className="tiles">
-                {tiles.map((tile) => (
-                    <Tile key={tile.id} {...tile} />
-                ))}
-            </div>
-        </div>
-    ) : null;
+  const { name, tiles, id } = props;
+  const changePlayerUrl = `${window.location.pathname
+    .split('/')
+    .slice(0, 3)
+    .join('/')}/${id}`;
+  return tiles.length > 0 ? (
+    <div className="player" title={name}>
+      <a className="name" href={changePlayerUrl}>
+        {name}
+        <svg width="2vw" height="2vh" xmlns="http://www.w3.org/2000/svg">
+          <use href="#share" />
+        </svg>
+      </a>
+      <div className="tiles">
+        {tiles.map((tile) => (
+          <Tile key={tile.id} {...tile} />
+        ))}
+      </div>
+    </div>
+  ) : null;
 };
 
 PlayerTiles.displayName = 'Player Tiles';
