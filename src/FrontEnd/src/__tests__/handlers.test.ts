2ef76d2c6e027deed6244df5cd4fffc3510fe11f
Sol <sol.jennings@myob.com>
2021-02-14 21:13:39 +1100

    clean

--- src/FrontEnd/src/__tests__/handlers.test.ts
+++ src/FrontEnd/src/__tests__/handlers.test.ts
@@ -25,31 +25,36 @@ describe(`handler tests`, () => {
       document.body.append(container);
       const elements = container.getElementsByTagName('div');
       [div1, div2, div3] = Array.from(elements);
+      jest.spyOn(div1, 'focus');
       jest.spyOn(div2, 'focus');
+      jest.spyOn(div3, 'focus');
     });
 
     test('should move to next element on keydown', () => {
-      div1.focus();
       handleKeyboardNav({ key: 'ArrowDown', target: div1 });
       expect(div2.focus).toBeCalled();
     });
 
     test('should move to next element on key right', () => {
-      div1.focus();
       handleKeyboardNav({ key: 'ArrowRight', target: div1 });
       expect(div2.focus).toBeCalled();
     });
 
     test('should move to next element on key up', () => {
-      div1.focus();
       handleKeyboardNav({ key: 'ArrowUp', target: div3 });
       expect(div2.focus).toBeCalled();
     });
 
     test('should move to next element on key left', () => {
-      div1.focus();
       handleKeyboardNav({ key: 'ArrowLeft', target: div3 });
       expect(div2.focus).toBeCalled();
+    });
+    
+    test('should not move on other keys', () => {
+      handleKeyboardNav({ key: 'n', target: div1 });
+      expect(div1.focus).not.toBeCalled();
+      expect(div2.focus).not.toBeCalled();
+      expect(div3.focus).not.toBeCalled();
     });
   });
 });
