51d6a55a516c1c82691ac2c941f586c545e97c06
Sol <sol.jennings@myob.com>
2021-02-20 01:48:19 +1100

    coverage

--- src/FrontEnd/src/handlers.ts
+++ src/FrontEnd/src/handlers.ts
@@ -11,14 +11,14 @@ export function handleDrop(ev: { preventDefault: () => void }): boolean {
 export const isEnterOrSpace = (event: KeyboardEvent): boolean => event.key === 'Enter' || event.key === ' ';
 
 export const handleKeyboardNav = (e: Pick<KeyboardEvent, 'key' | 'target'>): boolean => {
-  if (['ArrowDown', 'ArrowRight'].includes(e.key) && e.target) {
+  if ( e.target && 'ArrowDown'===e.key||'ArrowRight'===e.key ) {
     const allTabbable = Array.from(document.querySelectorAll('*[tabindex]:not(.name)'));
     const index = allTabbable.indexOf(e.target as HTMLElement);
     (allTabbable[(index + 1) % allTabbable.length] as HTMLElement).focus();
     return true;
   }
 
-  if (['ArrowUp', 'ArrowLeft'].includes(e.key) && e.target) {
+  if (e.target && 'ArrowUp'===e.key||'ArrowLeft'===e.key) {
     const allTabbable = Array.from(document.querySelectorAll('*[tabindex]:not(.name)'));
     const index = allTabbable.indexOf(e.target as HTMLElement);
     (allTabbable[(index + allTabbable.length - 1) % allTabbable.length] as HTMLElement).focus();
