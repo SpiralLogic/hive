349452cd923741cb3f29957bb9fffadfa140b09c
Sol <sol.jennings@myob.com>
2021-02-20 01:51:01 +1100

    coverage

--- src/FrontEnd/src/handlers.ts
+++ src/FrontEnd/src/handlers.ts
@@ -1,29 +1,29 @@
-export function handleDragOver(ev: { preventDefault: () => void }): boolean {
-  ev.preventDefault();
-  return false;
+export function handleDragOver (ev: { preventDefault: () => void }): boolean {
+    ev.preventDefault();
+    return false;
 }
 
-export function handleDrop(ev: { preventDefault: () => void }): boolean {
-  ev.preventDefault();
-  return false;
+export function handleDrop (ev: { preventDefault: () => void }): boolean {
+    ev.preventDefault();
+    return false;
 }
 
 export const isEnterOrSpace = (event: KeyboardEvent): boolean => event.key === 'Enter' || event.key === ' ';
 
 export const handleKeyboardNav = (e: Pick<KeyboardEvent, 'key' | 'target'>): boolean => {
-  if ( e.target && 'ArrowDown'===e.key||'ArrowRight'===e.key ) {
-    const allTabbable = Array.from(document.querySelectorAll('*[tabindex]:not(.name)'));
-    const index = allTabbable.indexOf(e.target as HTMLElement);
-    (allTabbable[(index + 1) % allTabbable.length] as HTMLElement).focus();
-    return true;
-  }
+    if (!e.target) return false;
+    if ('ArrowDown' === e.key || 'ArrowRight' === e.key) {
+        const allTabbable = Array.from(document.querySelectorAll('*[tabindex]:not(.name)'));
+        const index = allTabbable.indexOf(e.target as HTMLElement);
+        (allTabbable[(index + 1) % allTabbable.length] as HTMLElement).focus();
+        return true;
+    }
 
-  if (e.target && 'ArrowUp'===e.key||'ArrowLeft'===e.key) {
-    const allTabbable = Array.from(document.querySelectorAll('*[tabindex]:not(.name)'));
-    const index = allTabbable.indexOf(e.target as HTMLElement);
-    (allTabbable[(index + allTabbable.length - 1) % allTabbable.length] as HTMLElement).focus();
-    return true;
-  }
-  
-  return false;
+    if ('ArrowUp' === e.key || 'ArrowLeft' === e.key) {
+        const allTabbable = Array.from(document.querySelectorAll('*[tabindex]:not(.name)'));
+        const index = allTabbable.indexOf(e.target as HTMLElement);
+        (allTabbable[(index + allTabbable.length - 1) % allTabbable.length] as HTMLElement).focus();
+        return true;
+    }
+    return false;
 };
