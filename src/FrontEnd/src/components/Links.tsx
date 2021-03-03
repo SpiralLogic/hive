bdde25991080e9c695d36b4757ab0f6845d00806
Sol Jennings <sol.jennings@myob.com>
2021-03-03 20:54:33 +1100

    2 links

--- src/FrontEnd/src/components/Links.tsx
+++ src/FrontEnd/src/components/Links.tsx
@@ -2,10 +2,17 @@ import { FunctionComponent, h } from 'preact';
 
 export default () => {
   return (
-    <a class="github" href="https://github.com/SpiralLogic/hive" title="Visit the source code">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
-        <use href="#github" />
-      </svg>
-    </a>
+    <div class="links">
+      <a class="github" href="https://github.com/SpiralLogic/hive" title="Visit the source code">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+          <use href="#github" />
+        </svg>
+      </a>
+      <a class="github" href="https://github.com/SpiralLogic/hive" title="Visit the source code">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+          <use href="#github" />
+        </svg>
+      </a>
+    </div>
   );
 };
