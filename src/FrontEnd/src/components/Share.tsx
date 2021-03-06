cdcd53f01e53eb06ba9ea181eb25749e03ed080f
Sol <sol.jennings@myob.com>
2021-03-06 20:35:45 +1100

    fix clipboard

--- src/FrontEnd/src/components/Share.tsx
+++ src/FrontEnd/src/components/Share.tsx
@@ -3,8 +3,32 @@ import Modal from './Modal';
 
 type Props = { setShowShare: (value: boolean) => void };
 
+const fallbackCopyTextToClipboard = (text: string) => {
+  const currentFocus = document.activeElement as HTMLElement;
+  const textArea = document.createElement('textarea');
+  textArea.value = text;
+
+  textArea.style.top = '0';
+  textArea.style.left = '0';
+  textArea.style.position = 'fixed';
+
+  document.body.appendChild(textArea);
+  textArea.focus();
+  textArea.select();
+
+  try {
+    document.execCommand('copy');
+    if (currentFocus && currentFocus.focus) currentFocus.focus();
+    return true;
+  } catch (e) {
+    console.error(e);
+  }
+  document.body.removeChild(textArea);
+  return false;
+};
+
 const Share: FunctionComponent<Props> = (props) => {
-  const id = window.location.href.substring(0, -1) === '1' ? 1 : 0;
+  const id = window.location.href.substring(0, -1) === '1' ? 0 : 1;
   const opponentGame = {
     title: 'Hive board game',
     text: 'Share game to opponent!',
@@ -14,7 +38,11 @@ const Share: FunctionComponent<Props> = (props) => {
     navigator.share(opponentGame).then();
     return null;
   } catch {
-    navigator.clipboard.writeText(opponentGame.url).then();
+    if (navigator.clipboard) {
+      navigator.clipboard.writeText(opponentGame.url).then();
+    } else {
+      if (!fallbackCopyTextToClipboard(opponentGame.url)) return null;
+    }
   }
   return (
     <Modal name="rules" onClose={() => props.setShowShare(false)}>
@@ -22,6 +50,5 @@ const Share: FunctionComponent<Props> = (props) => {
     </Modal>
   );
 };
-
 Share.displayName = 'Share';
 export default Share;
