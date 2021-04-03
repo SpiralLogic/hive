40c60a83b4fae1a48b7e96b8096625cfba5ad7b8
Sol <sol.jennings@myob.com>
2021-04-03 21:35:36 +1100

    can see AI deciding

--- src/Domain/Hive.Domain/ComputerPlayer.cs
+++ src/Domain/Hive.Domain/ComputerPlayer.cs
@@ -62,7 +62,8 @@ namespace Hive.Domain
                 }
 
                 var score = Evaluate(nextMove) * depth;
-                if (CountQueenNeighbours().Any(c => c.Value > 0) ||
+                if ((depth==2 && CountQueenNeighbours().Any(c => c.Value > 0)) ||
+                    (stopWatch.Elapsed.Seconds < 10 && CountQueenNeighbours().Any(c => c.Value > 0)) ||
                     (stopWatch.Elapsed.Seconds < 10 && bestScore <= 0))
                 {
                     score += -(await Run(nextMove, depth - 1, stopWatch)).score;
