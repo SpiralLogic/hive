dc98776df66b823489176001aeab3d86e0f1e8d7
Sol <sol.jennings@myob.com>
2021-04-08 08:20:13 +1000

    ⬡await

--- src/Domain/Hive.Domain/Ai/Heuristics/GameOver.cs
+++ src/Domain/Hive.Domain/Ai/Heuristics/GameOver.cs
@@ -8,14 +8,9 @@ namespace Hive.Domain.Ai.Heuristics
         public int Get(HeuristicValues values, Move move)
         {
             if (values.GameStatus == GameStatus.Player0Win && move.Tile.PlayerId == 0 ||
-                values.GameStatus == GameStatus.Player1Win && move.Tile.PlayerId == 1)
+                values.GameStatus == GameStatus.Player1Win && move.Tile.PlayerId == 1||values.GameStatus == GameStatus.Draw)
             {
                 return 2*HeuristicValues.ScoreMax;
-            }
-
-            if (values.GameStatus == GameStatus.Draw)
-            {
-                return HeuristicValues.ScoreMax;
             }
 
             return 0;
