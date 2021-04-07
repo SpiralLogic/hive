9d25316bb6b3d1d8fda5b8a59ed614b95faf6742
Sol <sol.jennings@myob.com>
2021-04-07 22:04:49 +1000

    better ai

--- src/Domain/Hive.Domain/Ai/Heuristics/AntPlacement.cs
+++ src/Domain/Hive.Domain/Ai/Heuristics/AntPlacement.cs
@@ -14,10 +14,10 @@ namespace Hive.Domain.Ai.Heuristics
 
         public int Get(HeuristicValues values, Move move)
         {
-            if (_previousMoves.Peek().Coords != null) return 0;
             var ((_, _, creature), _) = move;
-            if (creature == Creatures.Ant && values.TilesPlaced > 3) return 2 * values.MoveNeighbours.Length;
-            return 0;
+            if (creature != Creatures.Ant ||_previousMoves.Peek().Coords != null) return 0;
+            if (values.TilesPlaced > 3) return 2 * values.MoveNeighbours.Length;
+            return -1;
         }
     }
 }
