fb6c884f19672824c4681fa74e3f66c02ee8f8ed
Spiral Logic <sol@sorijen.com.au>
2021-07-10 03:08:44 +1000

    lint

--- src/FrontEnd/test/rules/RuleCell.test.tsx
+++ src/FrontEnd/test/rules/RuleCell.test.tsx
@@ -1,8 +1,9 @@
+import { render } from '@testing-library/preact';
 import { h } from 'preact';
 import RuleCell from '../../src/components/rules/RuleCell';
 
 describe('rule cell snapshot tests', () => {
   it('snapshot', () => {
-    expect(<RuleCell />).toMatchSnapshot();
+    expect(render(<RuleCell />).baseElement).toMatchSnapshot();
   });
 });
