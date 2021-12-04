a32ad8c51b4c6a4774192a6ef4499998fd8a4b67
SpiralLogic <474197+SpiralLogic@users.noreply.github.com>
2021-12-05 08:30:14 +1100

    Update dotnet-update.sh

--- ops/dotnet-update.sh
+++ ops/dotnet-update.sh
@@ -1,33 +1,17 @@
 #!/bin/bash
-sed -i "s~#{image}~$ARTIFACT_IMAGE~g" ops/k8s/deployment.yaml
-
-if [ -z $KUBE_TOKEN ]; then
-  echo "FATAL: Environment Variable KUBE_TOKEN must be specified."
-  exit ${2:-1}
-fi
-
-if [ -z $NAMESPACE ]; then
-  echo "FATAL: Environment Variable NAMESPACE must be specified."
-  exit ${2:-1}
-fi
-
-echo
-echo "Namespace $NAMESPACE"
-
-status_code=$(curl -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
-    "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/apis/apps/v1beta2/namespaces/$NAMESPACE/deployments/hive-deployment" \
-    -X GET -o /dev/null -w "%{http_code}")
-
-if [ $status_code == 200 ]; then
-  echo
-  echo "Updating deployment"
-  curl --fail -H 'Content-Type: application/strategic-merge-patch+json' -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
-    "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/apis/apps/v1beta2/namespaces/$NAMESPACE/deployments/hive-deployment" \
-    -X PATCH -d @ops/k8s/deployment.yaml
-else
- echo
- echo "Creating deployment"
- curl --fail -H 'Content-Type: application/json' -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
-    "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/apis/apps/v1beta2/namespaces/$NAMESPACE/deployments" \
-    -X POST -d @ops/k8s/deployment.yaml
-fi
+regex='PackageReference Include="([^"]*)" Version="([^"]*)"'
+find . -name "*.*proj" | while read proj
+do
+  while read line
+  do
+    if [[ $line =~ $regex ]]
+    then
+      name="${BASH_REMATCH[1]}"
+      version="${BASH_REMATCH[2]}"
+      if [[ $version != *-* ]]
+      then
+        dotnet add $proj package $name
+      fi
+    fi
+  done < $proj
+done
