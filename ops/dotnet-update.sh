fd12222ae434e5a5198aa321aa98e9889b953ae1
SpiralLogic <474197+SpiralLogic@users.noreply.github.com>
2021-12-05 02:36:03 +1100

    Update dotnet-update.sh

--- ops/dotnet-update.sh
+++ ops/dotnet-update.sh
@@ -1,17 +1,57 @@
 #!/bin/bash
-regex='PackageReference Include="([^"]*)" Version="([^"]*)"'
-find . -name "*.*proj" | while read proj
-do
-  while read line
-  do
-    if [[ $line =~ $regex ]]
-    then
-      name="${BASH_REMATCH[1]}"
-      version="${BASH_REMATCH[2]}"
-      if [[ $version != *-* ]]
-      then
-        dotnet add $proj package $name
-      fi
-    fi
-  done < $proj
-done
+sed -i "s~#{image}~$ARTIFACT_IMAGE~g" ops/k8s/deployment.yaml
+
+if [ -z $KUBE_TOKEN ]; then
+  echo "FATAL: Environment Variable KUBE_TOKEN must be specified."
+  exit ${2:-1}
+fi
+
+if [ -z $NAMESPACE ]; then
+  echo "FATAL: Environment Variable NAMESPACE must be specified."
+  exit ${2:-1}
+fi
+
+echo
+echo "Namespace $NAMESPACE"
+
+status_code=$(curl -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
+    "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/apis/apps/v1beta2/namespaces/$NAMESPACE/deployments/bulletin-board-deployment" \
+    -X GET -o /dev/null -w "%{http_code}")
+
+if [ $status_code == 200 ]; then
+  echo
+  echo "Updating deployment"
+  curl --fail -H 'Content-Type: application/strategic-merge-patch+json' -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
+    "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/apis/apps/v1beta2/namespaces/$NAMESPACE/deployments/bulletin-board-deployment" \
+    -X PATCH -d @ops/k8s/deployment.yaml
+else
+ echo
+ echo "Creating deployment"
+ curl --fail -H 'Content-Type: application/json' -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
+    "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/apis/apps/v1beta2/namespaces/$NAMESPACE/deployments" \
+    -X POST -d @ops/k8s/deployment.yaml
+fi
+
+status_code=$(curl -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
+    "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/api/v1/namespaces/$NAMESPACE/services/bulletin-board-service" \
+    -X GET -o /dev/null -w "%{http_code}")
+
+if [ $status_code == 404 ]; then
+ echo
+ echo "Creating service"
+ curl --fail -H 'Content-Type: application/json' -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
+    "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/api/v1/namespaces/$NAMESPACE/services" \
+    -X POST -d @ops/k8s/deployment.yaml
+fi
+
+status_code=$(curl -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
+    "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/apis/extensions/v1beta1/namespaces/$NAMESPACE/ingresses/bulletin-board-ingress" \
+    -X GET -o /dev/null -w "%{http_code}")
+
+if [ $status_code == 404 ]; then
+ echo
+ echo "Creating ingress"
+ curl --fail -H 'Content-Type: application/json' -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
+    "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/apis/extensions/v1beta1/namespaces/$NAMESPACE/ingresses" \
+    -X POST -d @ops/k8s/deployment.yaml
+fi
