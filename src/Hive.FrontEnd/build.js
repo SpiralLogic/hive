fab339787f9473ce180459267ebd1a350ccf0db8
SpiralLogic <sol@sorijen.com.au>
2022-01-23 02:24:40 +1100

    change esbuild watch stuff

--- src/Hive.FrontEnd/build.js
+++ src/Hive.FrontEnd/build.js
@@ -1,14 +1,14 @@
 require('esbuild').buildSync({
-    entryPoints: ['./src/index.tsx'],
-    bundle: true,
-    define: {'process.env.NODE_ENV': `"${process.env.NODE_ENV}"` },
-    inject: ['./inject.shim.js'],
-    loader: {
-        '.svg': 'text',
-    },
-    minify: process.env.NODE_ENV !== 'development',
-    outfile: './public/js/hive.js',
-    sourcemap: process.env.NODE_ENV === 'development',
-    jsxFactory: 'h', jsxFragment: 'Fragment',
-    watch: !!process.env.WATCH
-})
+  entryPoints: ['./src/index.tsx'],
+  bundle: true,
+  define: { 'process.env.NODE_ENV': `"${process.env.NODE_ENV}"` },
+  inject: ['./inject.shim.js'],
+  loader: {
+    '.svg': 'text',
+  },
+  minify: process.env.NODE_ENV !== 'development',
+  outfile: './public/js/hive.js',
+  sourcemap: process.env.NODE_ENV === 'development',
+  jsxFactory: 'h',
+  jsxFragment: 'Fragment',
+});
