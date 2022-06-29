abeca0b70e5f93ccba1cd79698d852469a84df1a
Sol Jennings <sol@sorijen.com.au>
2022-06-30 08:57:07 +1000

    Do some more with the crappy MSBuild JS options

--- src/Hive.Api/Program.cs
+++ src/Hive.Api/Program.cs
@@ -1,8 +1,6 @@
 using System.Diagnostics.CodeAnalysis;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.FileProviders;
-using Microsoft.Extensions.Hosting;
 
 namespace Hive.Api;
 
@@ -20,15 +18,7 @@ public static class Program
     public static IWebHostBuilder CreateWebHostBuilder(string[] args)
     {
         return WebHost.CreateDefaultBuilder(args)
-            .ConfigureAppConfiguration(
-                (context, _) =>
-                {
-                    if (context.HostingEnvironment.IsProduction()) return;
-
-                    var webRoot = context.HostingEnvironment.ContentRootPath + "/wwwroot";
-                    context.HostingEnvironment.WebRootFileProvider = new PhysicalFileProvider(webRoot);
-                }
-            )
+          
             .UseStartup<Startup>();
     }
 }
