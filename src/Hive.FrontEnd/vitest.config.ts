fb823f7b1f4a8b9daf7c2a5a88a2955f88f9aed8
Sol Jennings <sol@sorijen.com.au>
2022-11-09 11:45:55 +1100

    upgrade eslint

--- src/Hive.FrontEnd/vitest.config.ts
+++ src/Hive.FrontEnd/vitest.config.ts
@@ -12,6 +12,7 @@ export default mergeConfig(
       include: ['**/src/*.test.*'],
       exclude: ['./src/__snapshots__/**'],
       setupFiles: ['./vitest.setup.ts'],
+      outputFile: '../reports/__tests__/test-results.xml',
       typecheck: { checker: 'tsc' },
       clearMocks: true,
       environment: 'jsdom',
@@ -19,6 +20,7 @@ export default mergeConfig(
       globals: true,
       reporters: ['dot'],
       coverage: {
+        enabled: true,
         reporter: ['text', 'json', 'html'],
         reportsDirectory: '../reports/__coverage__',
       },
