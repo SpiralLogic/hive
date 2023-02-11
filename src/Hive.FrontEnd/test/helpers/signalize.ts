e926f3618d7f32e9299c6cc763ace5f9ab3478d7
Sol Jennings <sol@sorijen.com.au>
2023-02-11 23:28:17 +1100

    all in on signals

--- src/Hive.FrontEnd/test/helpers/signalize.ts
+++ src/Hive.FrontEnd/test/helpers/signalize.ts
@@ -1,8 +1,8 @@
 import { Signal, signal } from '@preact/signals';
 
-type Signalized<T extends {}> = { [Property in keyof T]: Signal<T[Property]> };
+type Signalized<T extends Record<string, unknown>> = { [Property in keyof T]: Signal<T[Property]> };
 
-export const signalize = <T extends {}>(o: T) =>
+export const signalize = <T extends Record<string, unknown>>(o: T) =>
   Object.entries(o).reduce<Signalized<T>>(
     (testGameState, [k, v]) => ({
       ...testGameState,
