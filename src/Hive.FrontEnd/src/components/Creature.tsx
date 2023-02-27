febd5a74b0b519336f6a9833db748cf8b7cce878
Sol Jennings <sol@sorijen.com.au>
2023-02-27 23:07:52 +1100

    remove used code

--- src/Hive.FrontEnd/src/components/Creature.tsx
+++
@@ -1,8 +0,0 @@
-import { FunctionComponent } from 'preact';
-import type { Creature as CreatureType } from '../domain/creature';
-
-export const Creature: FunctionComponent<{ creature: CreatureType }> = ({ creature }) => (
-  <use className="creature" href={`#${creature.toLowerCase()}`} />
-);
-Creature.displayName = 'Creature';
-export default Creature;
