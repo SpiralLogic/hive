7819a7ff7ec8b1984a7bc5513ded2cf2feb1c418
Sol Jennings <sjennings@bunnings.com.au>
2023-05-18 20:49:31 +1000

    stuff

--- src/Hive.FrontEnd/src/index.tsx
+++ src/Hive.FrontEnd/src/index.tsx
@@ -1,9 +1,8 @@
 import { h, render } from 'preact';
-import App from './components/App';
-import { serverConnectionFactory } from './services';
-import creatures from './svg/creatures.svg?raw';
-
-import GameEngine from './services/game-engine';
+import App from '@hive/components/App';
+import { serverConnectionFactory } from '@hive/services';
+import GameEngine from '@hive/services/game-engine';
+import creatures from '@hive/svg/creatures.svg';
 
 document.body.insertAdjacentHTML('beforeend', `${creatures}`);
 document.title = import.meta.env.HIVE_APP_TITLE;
