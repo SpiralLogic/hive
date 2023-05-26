c89cfa7bd8b8625c7fdc58c1c3620b12a68b56e6
Sol <sol@sorijen.com.au>
2023-05-27 00:50:40 +1000

    MaxSearchTime       LANG: en_US.UTF-8       DOTNET_SYSTEM_GLOBALIZATION_INVARIANT: 1

--- src/Hive.Domain.Tests/ComputerPlayerTests.cs
+++ src/Hive.Domain.Tests/ComputerPlayerTests.cs
@@ -704,6 +704,6 @@ public class ComputerPlayerTests
         await hive.AiMove((_, _) => ValueTask.CompletedTask);
 
         stopwatch.Stop();
-        stopwatch.ElapsedMilliseconds.Should().BeLessThan(2300);
+        stopwatch.ElapsedMilliseconds.Should().BeLessThan(5300);
     }
 }
