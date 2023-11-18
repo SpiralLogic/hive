78c7d01b0eb04758f4e69e12c5631fa94acbc895
Sol <sol@sorijen.com.au>
2023-11-19 04:09:37 +1100

    CA1862

--- src/Hive.Domain.Tests/TestUtils/InitialHiveBuilder.cs
+++ src/Hive.Domain.Tests/TestUtils/InitialHiveBuilder.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using Hive.Domain.Entities;
 
 namespace Hive.Domain.Tests.TestUtils;
@@ -33,7 +34,7 @@ internal sealed class InitialHiveBuilder : HiveBuilder
         else
         {
             var (creature, cellString, _) = AllSymbols.Single(s => s.Symbol == symbol);
-            var playerId = cellString.ToString().ToUpperInvariant() == cellString.ToString() ? 0 : 1;
+            var playerId = cellString.ToString().Equals(cellString.ToString().ToUpperInvariant(), StringComparison.InvariantCulture) ? 0 : 1;
             cell.AddTile(new Tile(TileIdSequence++, playerId, creature));
         }
     }
