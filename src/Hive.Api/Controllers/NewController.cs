b05bd66f1d052bbeffcf31214a6f469511503562
Sol <sol@sorijen.com.au>
2023-12-26 02:24:38 +1100

    decocuple engine from signals

--- src/Hive.Api/Controllers/NewController.cs
+++ src/Hive.Api/Controllers/NewController.cs
@@ -9,7 +9,6 @@ using Hive.Domain.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Distributed;
 using Microsoft.Extensions.Options;
-using Unleash;
 
 namespace Hive.Api.Controllers;
 
@@ -17,30 +16,27 @@ namespace Hive.Api.Controllers;
 public class NewController : Controller
 {
     private readonly IDistributedCache _distributedCache;
-    private readonly IUnleash _unleash;
     private readonly JsonSerializerOptions _jsonSerializerOptions;
     private static readonly string[] PlayerNames={
         "P1",
         "P2"
     };
 
-    public NewController(IOptions<JsonOptions> jsonOptions, IDistributedCache distributedCache, IUnleash unleash)
+    public NewController(IOptions<JsonOptions> jsonOptions, IDistributedCache distributedCache)
     {
         ArgumentNullException.ThrowIfNull(jsonOptions);
 
         _distributedCache = distributedCache;
-        _unleash = unleash;
         _jsonSerializerOptions = jsonOptions.Value.JsonSerializerOptions;
     }
 
     [HttpPost]
     [Route("api/new")]
     [Produces("application/json")]
-    public async ValueTask<CreatedAtRouteResult> Post(IUnleash unleash)
+    public async ValueTask<CreatedAtRouteResult> Post()
     {
         var gameId = new string(HttpContext.TraceIdentifier.Split(":")[0].ToCharArray().OrderBy(_ => Guid.NewGuid()).ToArray());
 
-        Console.WriteLine(_unleash.IsEnabled("test"));
         var newGame = HiveFactory.Create(
             PlayerNames
         );
